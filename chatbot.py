88a3f269ce529911f20ee311ba9a1bf138a69199
Adithyakaushik326 <53081975+Adithyakaushik326@users.noreply.github.com>
2020-01-28 14:28:48 +0530

    Updated

--- chatbot.py
+++ chatbot.py
@@ -3,9 +3,9 @@ from chatterbot.trainers import ListTrainer
 import os
 bot=ChatBot('Bot')
 trainer = ListTrainer(bot)
-# for files in os.listdir('D:/Projects/python practice/chatbot/chatterbot-corpus/chatterbot_corpus/data/english/'):
-# 	data=open('D:/Projects/python practice/chatbot/chatterbot-corpus/chatterbot_corpus/data/english/' + files ,'r').readlines()
-# 	trainer.train(data)
+for files in os.listdir('D:/Projects/python practice/chatbot/chatterbot-corpus/chatterbot_corpus/data/english/'):
+	data=open('D:/Projects/python practice/chatbot/chatterbot-corpus/chatterbot_corpus/data/english/' + files ,'r').readlines()
+	trainer.train(data)
 while True:
 	msg=input('you:')
 	if msg != 'bye':
