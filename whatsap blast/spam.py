5be760e860f916700605b300af79e54c522a5308
Adithyakaushik326 <adithyakaushik326@gmail.com>
2020-06-07 17:01:49 +0530

    started deep learning

---
+++ whatsap blast/spam.py
@@ -0,0 +1,20 @@
+import pyautogui as pgi
+a=[]
+# print(pgi.position())
+f = open('song.txt')
+# for i in range(1,4):
+
+# for l in f:
+#     a = l.split(" ")
+#     print(a)
+#     # print(l.strip())
+#     for w in a:
+#         # for letter in w:
+#         pgi.click(598,731)
+#         pgi.typewrite(w+"  .")
+#         pgi.click(1333,728)
+
+for i in range(100):
+    pgi.click(598,731)
+    pgi.typewrite("SHASHIKALAAAA!!")
+    pgi.click(1333,728)
